358aa20fe84c012c8934820f8c2992aac6d667af
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-05-08 15:50:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
-app.use('/static', express.static('public'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
-app.listen(3001, () => console.log(`Server is listening on port hhtp://localhost:3001`));
+app.listen(3001, () => console.log(`Server is listening on port http://localhost:3001`));
