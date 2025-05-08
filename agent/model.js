bc7cbe0d075e1fde229523f22d8f6856bdc04e91
Dilyana Valkova <dilyana.komitova@gmail.com>
2025-05-08 13:13:11 +0300

    Update model.js

--- agent/model.js
+++ agent/model.js
@@ -2,7 +2,7 @@ import { ChatOllama } from '@langchain/ollama';
 
 const llm = new ChatOllama({
     baseUrl: 'http://localhost:11434',
-    model: 'llama3.2'
+    model: 'gemma:2b'
 });
 
 export default llm;
